b61aa2972e0f15d61e6bd1b083a16683e7c1a223
IvanGerman <ivgvi@mail.ru>
2022-05-29 23:02:32 +0700

    feat: add sequenceMark array

--- src/components/startpage/ParseDataClass.js
+++ src/components/startpage/ParseDataClass.js
@@ -1,8 +1,11 @@
 export class ParseDataClass {
 
+  sequencesMark = []
+
   iterateArray(csvArray) {
     console.log('ParseDataClass');
 
+    let status;
     for (let i = (csvArray.length - 2); i > 0; i -= 1) {
 
 
@@ -19,21 +22,31 @@ export class ParseDataClass {
         //--------------------------------------------------------------------------
         for (let j = i; j > 0; j -= 1) {
           if ( (csvArray[i][1] === csvArray[j - 1][1]) & (csvArray[i][0] === csvArray[j - 1][0]) ) {
-            console.log('its reload case  ParseDataClass', i, j - 1);
+            console.log('its reload case  ParseDataClass', i, j - 1, 'status-',status);
+            status = 'reload';
+            this.sequencesMark.push(['reload', i, j - 1]);
             break;
+          } else {
+            // status = 'otherGame';
+            // console.log('its otherGame case  ParseDataClass', i, j - 1, 'status-',status);
+            // this.sequencesMark.push(['otherGame', i]);
           }
         };
-
+        status = 'otherGame';
+        console.log('its otherGame case  ParseDataClass', i, 'status-',status);
+        this.sequencesMark.push(['otherGame', i]);
 
 
 
         
       //--------------------------------------------------------------------------  
-      } else {
-        console.log('its 1 game in 1 file case  ParseDataClass');
+      } else { //single game or many reloads/remaps at the same turn
+        status = 'singleGame';
+        console.log('its 1 game in 1 file case  ParseDataClass', 'status-',status);
           
       };
     }
+    console.log('this.sequencesMark--',this.sequencesMark);
   }
 }
 

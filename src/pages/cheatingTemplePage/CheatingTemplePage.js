acf6cef7a5d2cb9f0d4fa5cb7a9daccc641a1fba
IvanGerman <ivgvi@mail.ru>
2023-05-06 15:17:38 +0700

    feat: divide city states by groups/colors

--- src/pages/cheatingTemplePage/CheatingTemplePage.js
+++ src/pages/cheatingTemplePage/CheatingTemplePage.js
@@ -1,12 +1,19 @@
 //import { CheatingTemplePageClass } from '../../components/cheatingTemplePage/CheatingTemplePageClass';
-import { data } from '../../state/data';
+import { blueCityStates, data, goldCityStates, orangeCityStates, pinkCityStates, redCityStates, whiteCityStates } from '../../state/data';
 import './CheatingTemplePage.scss';
 
 const CheatingTemplePage = {
   render: async () => {
     const view = `
     <div class="cheatingTempleWrapper">
-      <div class="csWrapper">${data.allCityStates}</div>
+      <div class="csWrapper">
+        <div class="goldcs citystates"></div>
+        <div class="bluecs citystates"></div>
+        <div class="pinkcs citystates"></div>
+        <div class="redcs citystates"></div>
+        <div class="whitecs citystates"></div>
+        <div class="orangecs citystates"></div>
+      </div>
       <img src="./../../assets/img/cheatingtemple-bg2.jpg" alt="" />
     <div>
     `;
@@ -15,6 +22,34 @@ const CheatingTemplePage = {
   after_render: async () => {
     console.log('CheatingTemplePageClass rendered', data.allCityStates);
     //new CheatingTemplePageClass();  
+    const allGoldCS = data.allCityStates.filter((elem) => {
+      return (goldCityStates.includes(elem));
+    });
+    const goldcsWrapper = document.querySelector('.goldcs');
+    goldcsWrapper.innerHTML = [...allGoldCS];
+    console.log('allGoldCS--',allGoldCS);
+    const allBlueCS = data.allCityStates.filter((elem) => {
+      return (blueCityStates.includes(elem));
+    });
+    console.log('allBlueCS--',allBlueCS);
+    const allRedCS = data.allCityStates.filter((elem) => {
+      return (redCityStates.includes(elem));
+    });
+    console.log('allRedCS--',allRedCS);
+    const allWhiteCS = data.allCityStates.filter((elem) => {
+      return (whiteCityStates.includes(elem));
+    });
+    console.log('allWhiteCS--',allWhiteCS);
+    const allOrangeCS = data.allCityStates.filter((elem) => {
+      return (orangeCityStates.includes(elem));
+    });
+    const orangecsWrapper = document.querySelector('.orangecs');
+    orangecsWrapper.innerHTML = [...allOrangeCS];
+    console.log('allOrangeCS--',allOrangeCS);
+    const allPinkCS = data.allCityStates.filter((elem) => {
+      return (pinkCityStates.includes(elem));
+    });
+    console.log('allPinkCS--',allPinkCS);
   },
 };
 
